7340a6488d6ddc837a5afb620dc88cbce0b71562
kratman <kratmandu@gmail.com>
2023-07-16 11:41:23 -0400

    Adding a classification function

--- src/Perceptron.kt
+++ src/Perceptron.kt
@@ -1,13 +1,25 @@
 package src
 
 class Perceptron(size: Int) {
-    val model: MutableList<Double>
+    private val model: MutableList<Double>
+    private var bias: Double
 
     init {
         model = MutableList(size) {0.0}
+        bias = 0.0
     }
 
-    fun trainingStep() {
+    private fun trainingStep() {
 
+    }
+
+    fun classify(features: MutableList<Double>): Boolean {
+        assert(features.size != model.size) {"Incorrect number of features"}
+        var sum = 0.0
+        for (i in 0 until model.size) {
+            sum += model[i] * features[i]
+        }
+        sum += bias
+        return sum > 0
     }
 }
