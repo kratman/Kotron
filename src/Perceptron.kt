51fb03c2447bd7bfa86498421ffc278d9db03269
kratman <kratmandu@gmail.com>
2023-07-16 11:14:52 -0400

    First commit

---
+++ src/Perceptron.kt
@@ -0,0 +1,13 @@
+package src
+
+class Perceptron(size: Int) {
+    val model: MutableList<Double>
+
+    init {
+        model = MutableList(size) {0.0}
+    }
+
+    fun trainingStep() {
+
+    }
+}
